c2b738011b67e0641d720ac4e92b1e247b5210be
Spielberg <javiersanchezostiz@gmail.com>
2025-01-23 17:55:57 +0100

    nueva web + slider

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+var _body = "",
+  _html = "",
+  _htmlBody = "",
+  _window = "",
+  _layout = "",
+  isTocActive = false,
+  isTocRuning = false,
+  casesSlider = null;
+
+jQuery(document).ready(function ($) {
+
+  _body = jQuery('body');
+  _html = jQuery('html');
+  _htmlBody = jQuery('html, body');
+  _window = jQuery(window);
+  _layout = jQuery('.layout');
+
+  // default script
+  // jQuery('#icon_vgfx').load(THEME_URL+'assets/images/vgfx.svg');
+
+  // for handle mobile nav
+  jQuery('.nav-toggle-btn').on('click', function (e) {
+    e.preventDefault();
+    _html.toggleClass('nav-expanded');
+  });
+
+  jQuery('.la-backdrop').on('click', function (e) {
+    e.preventDefault();
+    _html.removeClass('nav-expanded');
+  });
+
+  // header menu add and removeclass
+  if (jQuery(window).width() <= 991) {
+    jQuery('.la-header-link').on('mouseenter', function () {
+      if (!navToggleBtnWrap.is(":visible")) {
+        var ele = jQuery(this);
+        jQuery('.la-header-nav-list > li').removeClass('active');
+        ele.parent().addClass('active');
+      }
+    });
+
+    jQuery('.la-header-nav-list > li').on('mouseleave', function () {
+      if (!navToggleBtnWrap.is(":visible")) {
+        jQuery('.la-header-nav-list > li').removeClass('active');
+      }
+    });
+  }
+
+
+  // for scrollTo the particular section
+  jQuery('.js-scrollto').on('click', function (e) {
+    e.preventDefault();
+    jQuery('.js-scrollto').parent().removeClass('active');
+
+    var ele = jQuery(this),
+      targetEle = jQuery(ele.attr('href'));
+
+    if (targetEle.length) {
+      ele.parent().addClass('active');
+      _htmlBody.stop(true, true).animate({ scrollTop: (targetEle.offset().top - jQuery('.site-header').height()) });
+    }
+  });
+
+});
